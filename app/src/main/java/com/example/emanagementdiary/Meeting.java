35cfbf2a4f82e55e146cf4d6924aaa159574f797
SzaboTamas <tomika1097@gmail.com>
2021-04-27 21:33:04 +0200

    Meeting Activity added

---
+++ app/src/main/java/com/example/emanagementdiary/Meeting.java
@@ -0,0 +1,14 @@
+package com.example.emanagementdiary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Meeting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meeting);
+    }
+}
--- app/src/main/java/com/example/emanagementdiary/foablakActivity.java
+++ app/src/main/java/com/example/emanagementdiary/foablakActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -34,6 +35,14 @@ public class foablakActivity extends AppCompatActivity {
             FirebaseAuth.getInstance().signOut();   //firebase sign out
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
+        });
+
+        findViewById(R.id.btn_meeting).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Meeting.class);
+                startActivity(intent);
+            }
         });
     }
 }
