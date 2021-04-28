91c66c5fe00c73f8f431ab46433da9c5c146ac4d
SzaboTamas <tomika1097@gmail.com>
2021-04-28 16:59:14 +0200

    A

--- app/src/main/java/com/example/emanagementdiary/Meeting.java
+++ app/src/main/java/com/example/emanagementdiary/Meeting.java
@@ -10,5 +10,6 @@ public class Meeting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meeting);
+
     }
 }
